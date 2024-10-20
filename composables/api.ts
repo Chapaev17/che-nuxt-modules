2efb421e83bbb63c1162af849d12b9d18bcb7c67
root <root@tula-term.local>
2024-10-20 14:02:22 +0000

    feat: added fetch detail api

--- composables/api.ts
+++ composables/api.ts
@@ -15,7 +15,35 @@ export function useListApi<ListData = {}[]>(parameters: { url: string }) {
         : isError(catchedError)
           ? catchedError.message
           : ""
-      fetchDataErrors.value = `Delete track error: ${message}`
+      fetchDataErrors.value = `Fetch data error: ${message}`
+      fetchDataErrors.value = "error"
+    }
+  }
+
+  return { data, fetchDataStatus, fetchDataErrors, fetchData }
+}
+
+export function useDetailApi<ResponseData = {}>(parameters: { url: string }) {
+  const data = ref<ResponseData>()
+  const fetchDataStatus = ref()
+  const fetchDataErrors = ref()
+
+  async function fetchData(fetchParameters: { id: string }) {
+    try {
+      data.value = undefined
+      fetchDataStatus.value = "pending"
+      const response: ResponseData = await $fetch(
+        `${parameters.url}/${fetchParameters.id}/`,
+      )
+      data.value = response
+      fetchDataStatus.value = "success"
+    } catch (catchedError) {
+      const message = isString(catchedError)
+        ? catchedError
+        : isError(catchedError)
+          ? catchedError.message
+          : ""
+      fetchDataErrors.value = `Fetch data error: ${message}`
       fetchDataErrors.value = "error"
     }
   }
