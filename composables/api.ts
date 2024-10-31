10618332bd7a0b7965d0437fc6349e68782b12ae
root <root@tula-term.local>
2024-10-31 21:23:17 +0000

    feat: added send form use

--- composables/api.ts
+++ composables/api.ts
@@ -6,6 +6,8 @@ export function useListApi<ListData = {}[]>(parameters: { url: string }) {
   async function fetchData() {
     try {
       fetchDataStatus.value = "pending"
+      data.value = undefined
+      fetchDataErrors.value = undefined
       const response: ListData = await $fetch(`${parameters.url}/`)
       data.value = response
       fetchDataStatus.value = "success"
@@ -32,8 +34,9 @@ export function useDetailApi<ResponseData = {}>(parameters: {
 
   async function fetchData(fetchParameters?: { id?: string }) {
     try {
-      data.value = undefined
       fetchDataStatus.value = "pending"
+      data.value = undefined
+      fetchDataErrors.value = undefined
       const response: ResponseData = await $fetch(
         fetchParameters?.id
           ? `${parameters.url}/${fetchParameters.id}/`
@@ -55,4 +58,68 @@ export function useDetailApi<ResponseData = {}>(parameters: {
   }
 
   return { data, fetchDataStatus, fetchDataErrors, fetchData }
+}
+
+export function useFormApi<Form>(parameters: {
+  url: string
+  method?: "patch" | "post"
+  blankForm?: Form
+}) {
+  type FormKeys = keyof Form
+  type FormErrors = {
+    [key in FormKeys | "non_field_errors"]?: string[] | undefined
+  }
+  const form = ref<Form>(parameters.blankForm || ({} as Form))
+  const sendFormStatus = ref()
+  const sendFormRequestErrors = ref()
+  const formErrors = ref<FormErrors>()
+
+  async function clearFormErrorsForKey(formErrorsKey: keyof FormErrors) {
+    if (formErrors.value?.[formErrorsKey])
+      formErrors.value[formErrorsKey] = undefined
+  }
+
+  async function sendForm(fetchParameters?: { id?: string; form: Form | {} }) {
+    try {
+      sendFormStatus.value = "pending"
+      sendFormRequestErrors.value = undefined
+      formErrors.value = undefined
+      await $fetch(
+        fetchParameters?.id
+          ? `${parameters.url}/${fetchParameters.id}/`
+          : parameters.url,
+        {
+          method: parameters.method || "patch",
+          body: fetchParameters?.form || form.value,
+          async onResponseError({ response }) {
+            if (response.status === 400) {
+              formErrors.value = response._data
+            }
+            sendFormRequestErrors.value = `Fetch data error`
+            sendFormStatus.value = "error"
+          },
+          onResponse() {
+            sendFormStatus.value = "success"
+          },
+        },
+      )
+    } catch (catchedError) { }
+  }
+
+  function reset() {
+    form.value = parameters.blankForm
+    sendFormStatus.value = undefined
+    sendFormRequestErrors.value = undefined
+    formErrors.value = undefined
+  }
+
+  return {
+    form,
+    sendFormStatus,
+    sendFormRequestErrors,
+    formErrors,
+    sendForm,
+    clearFormErrorsForKey,
+    reset,
+  }
 }
