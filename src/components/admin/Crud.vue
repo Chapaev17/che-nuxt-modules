3f9515519d4025623f29ef9c0faee15d8a7813c2
root <root@dev.local>
2026-07-17 19:35:26 +0000

    fix: prevent SSR rendering in Crud with onMounted guard

--- src/components/admin/Crud.vue
+++ src/components/admin/Crud.vue
@@ -1,4 +1,5 @@
 <script setup lang="tsx">
+import { ref, onMounted } from "vue"
 import type { MyOpenAPIDocument } from "../../stores/adminPanel/types"
 import SidebarEndpointsMenu from "./SidebarEndpointsMenu.vue"
 import ListElementByCurrentDevice from "./ListElementByCurrentDevice.vue"
@@ -14,19 +15,24 @@ interface Props {
 
 const props = defineProps<Props>()
 
+const isReady = ref(false)
+onMounted(() => { isReady.value = true })
+
 const adminPanelStore = useAdminPanelStore()
 adminPanelStore.setSchema(props.apiSchema)
 
 const { filteredEntitiesByNamespace } = storeToRefs(adminPanelStore)
 
-useRender(() => (
-  <div class="flex h-[calc(100vh-56px)] bg-white">
-    <SidebarEndpointsMenu
-      filteredEntitiesByNamespace={filteredEntitiesByNamespace.value}
-      isMobile={props.isMobile}
-    />
+useRender(() => isReady.value
+  ? (
+    <div class="flex h-[calc(100vh-56px)] bg-white">
+      <SidebarEndpointsMenu
+        filteredEntitiesByNamespace={filteredEntitiesByNamespace.value}
+        isMobile={props.isMobile}
+      />
 
-    <ListElementByCurrentDevice baseUrl={props.baseUrl} isMobile={props.isMobile} />
-  </div>
-))
+      <ListElementByCurrentDevice baseUrl={props.baseUrl} isMobile={props.isMobile} />
+    </div>
+  )
+  : null)
 </script>
